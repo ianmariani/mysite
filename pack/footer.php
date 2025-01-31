1f6a8abc73619ea6bccf983105b18d3013d30134
ianmariani <82089769+ianmariani@users.noreply.github.com>
2025-01-31 21:33:48 +0300

    footer.php

--- pack/footer.php
+++ pack/footer.php
@@ -4,7 +4,7 @@
          
         <div class="col-md-3 ">
             <font class="font-weight-bold">
-                Beta Virtuals
+                Equalgate Services
             </font><br>
             <a class="footer-link" href="what-we-do.php">What we do</a><br> 
             <a class="footer-link" href="about-us.php">About us</a><br> 
@@ -21,12 +21,12 @@
                 Useful link
             </font><br>
             <a class="footer-link" href="work.php?type=personal">Personal Admin</a><br> 
-            <a class="footer-link" href="work.php?type=social-media">Social media Management</a><br> 
-            <a class="footer-link" href="work.php?type=research">Research assistant</a><br> 
-            <a class="footer-link" href="work.php?type=ecommerce">E-commerce assistant</a><br> 
-            <a class="footer-link" href="work.php?type=project-manager">Project management</a><br>  
-            <a class="footer-link" href="work.php?type=event">Email management</a><br> 
-            <a  class="footer-link" href="work.php?type=email">Email managment</a> <br> 
+            <a class="footer-link" href="work.php?type=social-media">Social Media Management</a><br> 
+            <a class="footer-link" href="work.php?type=research">Research Assistant</a><br> 
+            <a class="footer-link" href="work.php?type=ecommerce">E-commerce Assistant</a><br> 
+            <a class="footer-link" href="work.php?type=project-manager">Project Management</a><br>  
+            <a class="footer-link" href="work.php?type=event">Email Management</a><br> 
+            <a  class="footer-link" href="work.php?type=email">Email Managment</a> <br> 
             <!-- <a  class="footer-link" href="work.php?type=travel">Travel managment</a> <br>  
             <a  class="footer-link" href="work.php?type=website">Website maintenance</a> <br>  
             <a  class="footer-link" href="work.php?type=managing-finance">Managing finance assistant</a>  --><br> <br> 
@@ -52,7 +52,7 @@
         <div class="col-md-1"></div>
         <div class="col-md-10 text-center">
             <hr style="height:0.4px; background-color:white;border:none">
-            <small>Beta Virtuals© Copyright 2023 - bs . All rights reserved.</small>
+            <small>Equalgate© Copyright 2025 - ES . All rights reserved.</small>
         </div>
         <div class="col-md-1"></div>
         
